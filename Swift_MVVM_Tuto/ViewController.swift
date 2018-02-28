dc30e812c278b0ba8be8f7b1083cb377ae9c526d
Darkkrye <pierre.boudon.27@gmail.com>
2018-02-28 11:46:35 +0100

    Initial Commit

---
+++ Swift_MVVM_Tuto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_MVVM_Tuto
+//
+//  Created by Openfield Mobility on 28/02/2018.
+//  Copyright © 2018 Openfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
